c75d12dc78f4c1da984ec7b30959617e37d5fde5
VLaxmikanth <laxmikanth.vattamvar@tibilsolutions.com>
2020-04-09 14:54:48 +0530

    leaflet code

--- development/leaflet/server-side/src/api/controller/adminLogin.js
+++ development/leaflet/server-side/src/api/controller/adminLogin.js
@@ -5,9 +5,15 @@ const jwt = require('jsonwebtoken');
 router.post('/', (req, res) => {
     var log = fs.readFileSync('./src/file/loginData.json');
     var auth = JSON.parse(log);
-    if (req.body.email !== auth.email) {
+    emails = [];
+    passwords = [];
+    for (i = 0; i < auth.length; i++) {
+        emails.push(auth[i].email);
+        passwords.push(auth[i].password);
+    }
+    if (!emails.includes(req.body.email)) {
         res.send({ notEmail: "User not found" });
-    } else if (req.body.cnfpass !== auth.password) {
+    } else if (req.body.cnfpass !== auth[0].password) {
         res.send({ wrongPasswd: "Wrong password" });
     } else {
         jwt.sign(auth, 'secret', { expiresIn: '24h' }, (err, data) => {
